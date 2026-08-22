dc52aa5b7ffd32cb7a28b0911e2d49fc67299860
Eliezer Miranda <E.92.MIRANDA@GMAIL.COM>
2026-08-22 15:24:48 +0200

    feat: allow HTTPS traffic on app NSG

--- spoke-nsg.tf
+++ spoke-nsg.tf
@@ -7,3 +7,16 @@ resource "azurerm_subnet_network_security_group_association" "app" {
   subnet_id                 = azurerm_subnet.app.id
   network_security_group_id = azurerm_network_security_group.app.id
 }
+resource "azurerm_network_security_rule" "allow_https" {
+  name                        = "Allow-HTTPS"
+  priority                    = 100
+  direction                   = "Inbound"
+  access                      = "Allow"
+  protocol                    = "Tcp"
+  source_port_range           = "*"
+  destination_port_range      = "443"
+  source_address_prefix       = "*"
+  destination_address_prefix  = "*"
+  resource_group_name         = azurerm_resource_group.hub.name
+  network_security_group_name = azurerm_network_security_group.app.name
+}
