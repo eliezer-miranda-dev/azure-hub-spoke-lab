ea1a32a0a481e7d632103979bca1b19944682f7e
Eliezer Miranda <E.92.MIRANDA@GMAIL.COM>
2026-08-22 13:56:40 +0200

    feat: add management subnet to hub network

--- subnets.tf
+++ subnets.tf
@@ -11,3 +11,10 @@ resource "azurerm_subnet" "gateway" {
   virtual_network_name = azurerm_virtual_network.hub.name
   address_prefixes     = ["10.0.2.0/24"]
 }
+
+resource "azurerm_subnet" "management" {
+  name                 = "ManagementSubnet"
+  resource_group_name  = azurerm_resource_group.hub.name
+  virtual_network_name = azurerm_virtual_network.hub.name
+  address_prefixes     = ["10.0.3.0/24"]
+}
