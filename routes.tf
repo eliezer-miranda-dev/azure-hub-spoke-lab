d29ea74aba814527f44cb139f8378946737821e0
Eliezer Miranda <E.92.MIRANDA@GMAIL.COM>
2026-08-22 14:48:53 +0200

    feat: associate route table with management subnet

--- routes.tf
+++ routes.tf
@@ -3,3 +3,8 @@ resource "azurerm_route_table" "hub" {
   location            = var.location
   resource_group_name = azurerm_resource_group.hub.name
 }
+
+resource "azurerm_subnet_route_table_association" "management" {
+  subnet_id      = azurerm_subnet.management.id
+  route_table_id = azurerm_route_table.hub.id
+}
