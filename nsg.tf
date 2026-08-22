8550505adbd20dd058545a5c8b37f0c1f7b2d566
Eliezer Miranda <E.92.MIRANDA@GMAIL.COM>
2026-08-22 14:27:13 +0200

    feat: add SSH security rule to management NSG

--- nsg.tf
+++ nsg.tf
@@ -8,3 +8,17 @@ resource "azurerm_subnet_network_security_group_association" "management" {
   subnet_id                 = azurerm_subnet.management.id
   network_security_group_id = azurerm_network_security_group.management.id
 }
+
+resource "azurerm_network_security_rule" "allow_ssh" {
+  name                        = "Allow-SSH"
+  priority                    = 100
+  direction                   = "Inbound"
+  access                      = "Allow"
+  protocol                    = "Tcp"
+  source_port_range           = "*"
+  destination_port_range      = "22"
+  source_address_prefix       = "*"
+  destination_address_prefix  = "*"
+  resource_group_name         = azurerm_resource_group.hub.name
+  network_security_group_name = azurerm_network_security_group.management.name
+}
