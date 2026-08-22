72c593f3f47e9ac510cb84ab9fe02041eea26de9
Eliezer Miranda <E.92.MIRANDA@GMAIL.COM>
2026-08-22 13:49:25 +0200

    feat: add Gateway subnet to hub network

--- subnets.tf
+++ subnets.tf
@@ -4,3 +4,10 @@ resource "azurerm_subnet" "firewall" {
   virtual_network_name = azurerm_virtual_network.hub.name
   address_prefixes     = ["10.0.1.0/24"]
 }
+
+resource "azurerm_subnet" "gateway" {
+  name                 = "GatewaySubnet"
+  resource_group_name  = azurerm_resource_group.hub.name
+  virtual_network_name = azurerm_virtual_network.hub.name
+  address_prefixes     = ["10.0.2.0/24"]
+}
